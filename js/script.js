c18a8d8a139b9d02bd9d257ece0a54f4b9b4e55c
orel1234x <154608621+orel1234x@users.noreply.github.com>
2023-12-22 15:53:45 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ================================ typing animation =================================== */
+var typed = new Typed(".typing",{
+    strings:["","association","Warehouses and caregivers of animals","Helping homeless animals","Helping Animals"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
